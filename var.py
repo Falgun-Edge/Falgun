9335c1c1289af68fae9ef764083ba120dd8a674d
Falgun-Edge <falgun.sharma@gammaedge.io>
2025-06-18 11:44:49 +0530

    add new file

---
+++ var.py
@@ -0,0 +1,24 @@
+# Understanding a variable 
+variable = "Hello I am learing python from scatch" # initialize a string to variable
+print(variable)
+print(type(variable))
+
+# data type integer
+num1 = 7 # initialize the number to variable num1
+num2 = 16 # initialize the number to variable num2
+total = num1+num2 # adding
+print(total)
+print(type(total))
+
+# data type float   
+value_1=9.34 # initialize the float value
+value_2= 77.3254  # initialize the float value
+sub = value_2-value_1 #substracting
+print(sub)
+print(type(sub))
+
+# data type Boolean
+print(num1<num2)
+print(num1==num2)
+print(type(num2>num1))
+
